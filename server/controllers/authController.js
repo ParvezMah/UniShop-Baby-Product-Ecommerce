c5b487762ed4df1bd9eb46d83d91493940bd5d0b
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2026-04-05 20:45:49 +0600

    part-1 : 48:00 : faces an error -> User already exists

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,12 +1,45 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 const registerUser = asyncHandler(async (req, res)=> {
     console.log("req", req.body);
     const { name, email, password, role } = req.body;
 
-    res.status(200).json({
-        message: "Register route is working",
+    const userExists = User.findOne({ email});
+
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists, Try login");
+    };
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role,
+        addresses: [],
+        // wishlist: [],
+        // cart: [],
+        // orders: [],
     })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            avatar: user.avatar,
+            role: user.role,
+            addresses: user.addresses,
+        })
+    } else {
+        res.status(400);
+        throw new Error("Invalid user Data");
+    }
+
+    // res.status(200).json({
+    //     message: "Register route is working",
+    // })
 });
 
 
