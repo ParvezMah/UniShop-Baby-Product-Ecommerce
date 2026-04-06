281219b0c777b36e11fa76d1d594bfcee430bb1a
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2026-04-06 09:58:13 +0600

    fix : Error : we did not add await before User.findOne({ email});

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,30 +5,12 @@ const registerUser = asyncHandler(async (req, res)=> {
     console.log("req", req.body);
     const { name, email, password, role } = req.body;
 
-    const userExists = User.findOne({ email});
+    const userExists = await User.findOne({ email});
 
     if (userExists) {
         res.status(400);
         throw new Error("User already exists, Try login");
     };
-
-    /*
-    part-1 : 48:00 : faces an error -> User already exists
-    Error in Postman
-    {
-        "message": "User already exists, Try login",
-        "stack": "Error: User already exists, Try login\n    at file:///D:/Youtube-Project/BabyMart/server/controllers/authController.js:12:15\n    
-        at asyncUtilWrap (D:\\Youtube-Project\\BabyMart\\server\\node_modules\\express-async-handler\\index.js:3:20)\n    
-        at Layer.handleRequest (D:\\Youtube-Project\\BabyMart\\server\\node_modules\\router\\lib\\layer.js:152:17)\n    
-        at next (D:\\Youtube-Project\\BabyMart\\server\\node_modules\\router\\lib\\route.js:157:13)\n    
-        at Route.dispatch (D:\\Youtube-Project\\BabyMart\\server\\node_modules\\router\\lib\\route.js:117:3)\n    
-        at handle (D:\\Youtube-Project\\BabyMart\\server\\node_modules\\router\\index.js:435:11)\n    
-        at Layer.handleRequest (D:\\Youtube-Project\\BabyMart\\server\\node_modules\\router\\lib\\layer.js:152:17)\n    
-        at D:\\Youtube-Project\\BabyMart\\server\\node_modules\\router\\index.js:295:15\n    
-        at processParams (D:\\Youtube-Project\\BabyMart\\server\\node_modules\\router\\index.js:582:12)\n    
-        at next (D:\\Youtube-Project\\BabyMart\\server\\node_modules\\router\\index.js:291:5)"
-    }
-    */
 
     const user = await User.create({
         name,
