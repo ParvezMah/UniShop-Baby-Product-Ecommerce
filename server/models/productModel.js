ce2a0aa7e54404af9fa254a9a0cfb434dd8d88c2
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2026-04-12 02:38:41 +0600

    part-1 | 02:50:00 | Tested createProduct, createBrand, createCategory api on Postman

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -58,7 +58,7 @@ const productSchema = new mongoose.Schema(
         },
         image: {
             type: String,
-            required: true,
+            // required: true,
         },
         category: {
             type: mongoose.Schema.Types.ObjectId,
