57e24ee31752239b2009c44df1a155b06529690b
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2026-04-13 18:14:50 +0600

    part-2 | 00:39:00 | Header design setup

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,21 +1,19 @@
 import { Outlet } from "react-router";
 import Header from "./components/Header";
-import { Toaster } from "./components/ui/sonner";
-import { Button } from "./components/ui/button";
 import Sidebar from "./components/Sidebar";
-import { toast } from "sonner";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
     <div className="h-screen flex bg-background">
       <Sidebar/>
-      <div className="flex flex-col flex-1 max-w-[--breakpoint-2xl] ml-64 ">
+      <div className="flex flex-col flex-1 max-w-[--breakpoint-2xl] ml-64">
         <Header/>
         <main>
           <Outlet/>
         </main>
       </div>
-      <Toaster/>
+      <Toaster position="bottom-right"/>
     </div>
   );
 }
--- admin/src/components/Header.tsx
+++ admin/src/components/Header.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className=''>Header</div>
+    <div className="sticky top-0 z-10 flex items-center h-16 gap-5 bg-background border-b border-border px-4">
+      Header
+      </div>
   )
 }
 
