4b412c6a902fc3a24e7f148526b87424b2f6f55b
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2026-04-13 06:09:28 +0600

    part-2 | 00:20:25 | all route component setup

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -2,18 +2,66 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import App from './App.tsx'
 import './index.css'
+import Login from './pages/Login.tsx';
+import Register from './pages/Register.tsx';
+import Dashboard from './pages/Dashboard.tsx';
+import Account from './pages/Account.tsx';
+import { Users } from 'lucide-react';
+import Orders from './pages/Orders.tsx';
+import Invoices from './pages/Invoices.tsx';
+import Products from './pages/Products.tsx';
+import Banners from './pages/Banners.tsx';
+import Categories from './pages/Categories.tsx';
+import Brands from './pages/Brands.tsx';
 
 const router = createBrowserRouter([
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
   {
     path: '/',
-    element: <App />
+    element: <App />,
+      children: [
+      {
+        index: true,
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/dashboard/account",
+        element: <Account />,
+      },
+      {
+        path: "/dashboard/users",
+        element: <Users />,
+      },
+      {
+        path: "/dashboard/orders",
+        element: <Orders />,
+      },
+      {
+        path: "/dashboard/invoices",
+        element: <Invoices />,
+      },
+      {
+        path: "/dashboard/products",
+        element: <Products />,
+      },
+      {
+        path: "/dashboard/banners",
+        element: <Banners />,
+      },
+      {
+        path: "/dashboard/categories",
+        element: <Categories />,
+      },
+      {
+        path: "/dashboard/brands",
+        element: <Brands />,
+      },
+    ],
   }
 ]);
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-  //   <App />
-  // </StrictMode>,
-
   <RouterProvider router={router} />
 )
