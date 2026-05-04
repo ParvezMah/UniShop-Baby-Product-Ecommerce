dc161866126d92a0570764d8de6cc143dd8576da
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2026-05-04 11:39:47 +0600

    chore: ignore build artifacts

--- client/.next/static/chunks/src_app_layout_tsx_68b267f5._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_globals_b805903d.css",
-  "static/chunks/node_modules_b43b92d1._.js",
-  "static/chunks/src_88e230b1._.js"
-],
-    source: "dynamic"
-});
